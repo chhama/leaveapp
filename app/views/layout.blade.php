7676dea7b5b463da59b4d98ddf0117c33775ead3
Chhama Chenkual <chhama5@gmail.com>
2014-12-13 09:24:09 +0530

    beautiful material design

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -16,41 +16,54 @@
 {{ HTML::Script('js/jquery-1.11.1.min.js') }}
 {{ HTML::Script('js/bootstrap.js') }}
 {{ HTML::Script('js/bootstrap-datetimepicker.js') }}
+{{ HTML::Script('js/ripples.min.js') }}
 {{ HTML::Script('js/material.min.js') }}
 {{ HTML::Script('js/npm.js') }}
-{{ HTML::Script('js/ripples.min.js') }}
 
+{{ HTML::Style('css/material.css') }}
 {{ HTML::Style('css/bootstrap.css') }}
 {{ HTML::Style('css/jquery.datetimepicker.css') }}
-<!-- {{ HTML::Style('css/custom.css') }} -->
-{{ HTML::Style('css/material.min.css') }}
 {{ HTML::Style('css/ripples.min.css') }}
 {{ HTML::Style('css/material-wfont.min.css') }}
+{{ HTML::Style('css/custom.css') }}
 
 @yield('extrajs')
 </head>
 <body>
-<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-	<div class="navbar-header col-md-3">
-    	<a class="navbar-brand" href="{{ URL::to('/')}}">Online Leave Application</a>
-  	</div>
-    <div class="col-md-7">
-    	<div class="collapse navbar-collapse">
-    		<ul class="nav navbar-nav">
-          <li><a href="{{ URL::route('leave.index')}}">Leave</a></li>
-          <li><a href="{{ URL::route('leavetaken.index')}}">Register Leave</a></li>
-    		</ul>
-    	</div>
-    </div>
-    <div class="col-md-2">
-      <div class="collapse navbar-collapse">
-        <ul class="nav navbar-nav pull-right">
-          <li><a href="{{ URL::route('user.index')}}">User</a></li>
-          <li><a href="{{ URL::to('logout')}}">Logout</a></li>
+<nav class="navbar navbar-inverse navbar-fixed-top shadow-z-5" role="navigation" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="{{ URL::to('/')}}">Online Leave Application</a>
+  </div>
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="{{ URL::route('leave.index')}}">Leave Type</a></li>
+      <li><a href="{{ URL::route('leavetaken.index')}}">Manage Leave</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li class="dropdown">
+
+        <a href="#" class="dropdown-toggle pull-left" data-toggle="dropdown"><i class="btn btn-warning btn-fab btn-raised mdi-action-perm-identity" style="margin-bottom:-40px;"><b class="caret"></b> </i></a>
+        <ul class="dropdown-menu">
+          <li><a href="{{ URL::route('user.index')}}">Profile <i class="mdi-action-account-circle"></i></a></li>
+          <li><a href="{{ URL::to('logout')}}">Logout <i class="mdi-action-exit-to-app"></i></a></li>
+     
         </ul>
-      </div>
-    </div>
+      </li>
+      <li><a href="#">&nbsp;</a></li>
+    </ul>
+  </div><!-- /.navbar-collapse -->
 </nav>
+
+
 <div class="container" style="margin-top:70px; margin-bottom:30px;">
   @if(Session::has('flash_message'))
       <p class="alert alert-success"><strong>{{ Session::get('flash_message') }}</strong></p>
@@ -58,7 +71,7 @@
 	@yield('container')
 </div>
 <footer class="dzFooter nav navbar-default text-center" style="position:fixed; bottom:0px; width:100%; height:50px; padding-top:15px; color:#000 ">
-     &copy;  <?=date('Y')?> 
+    Copyright &copy; MSeGS <?=date('Y')?> 
 </footer>
 </body>
 </html>
