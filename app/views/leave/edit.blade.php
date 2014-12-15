080e8850c094a668987d4d14fcfb84829f1092e9
John Dapzar <johndapzar@yahoo.com>
2014-12-15 12:26:09 +0530

    UI siam nual...tam mai..

--- app/views/leave/edit.blade.php
+++ app/views/leave/edit.blade.php
@@ -10,25 +10,26 @@
 <table width="100%" border="0" cellspacing="0" cellpadding="0" class="table table-hover" style="margin-bottom:0px;">
 <thead>
   <tr>
-    <th width="85" height="38" align="center">#</th>
-    <th width="244" height="38" align="left">Type</th>
-    <th width="244" height="38" align="left">Max Allowed</th>
-    <th width="121" align="left">Control</th>
+    <th width="85" height="38" class="text-center">#</th>
+    <th width="400" height="38" align="left">Type</th>
+    <th width="100" height="38" align="left">Max Allowed</th>
+    <th width="100" class="text-center">Control</th>
   </tr>
   </thead>
   <tbody>
   	<?php $slno = 0; ?>
   	@foreach($leaveAll as $leave)
   	<tr bgcolor="">
-    <td height="25" align="center">{{$slno+$index}}</td>
+    <td height="25" class="text-center">{{$slno+$index}}</td>
     <td height="25" align="left">{{ $leave->type }}&nbsp;</td>
-    <td height="25" align="left">{{ $leave->max_allowed }}&nbsp;</td>
+    <td height="25" class="text-center">{{ $leave->max_allowed }}&nbsp;</td>
+    {{Form::open(array('url'=>route('leave.destroy', array($leave->id)),'method'=>'delete'))}} 
     <td align="left" class="action text-center">
-    	{{Form::open(array('url'=>route('leave.destroy', array($leave->id)),'method'=>'delete'))}}
-            <a href="{{route('leave.edit', array($leave->id))}}" class="btn btn-xs btn-success tooltip-top" title="Edit leave"><i class="glyphicon glyphicon-edit"></i></a>&nbsp;&nbsp;
-            <button type="submit" onclick="return confirm ('<?php echo _('Are you sure') ?>');" name="id" class="btn btn-xs btn-danger tooltip-top" title="Remove Leave" value="{{$leave->id}}"><i class="glyphicon glyphicon-trash"></i></button>
-        {{Form::close()}}
+        <a href="{{route('leave.edit', array($leave->id))}}" title="Edit leave" class="btn btn-fab btn-fab-mini btn-raised btn-sm btn-primary"  style="width:30px; height:30px; padding:11px 5px 10px 6px;"><i class="glyphicon glyphicon-edit"></i></a>&nbsp;&nbsp;
+    
+        <button type="submit" onclick="return confirm ('<?php echo _('Are you sure') ?>');" name="id"  title="Remove Leave" value="{{$leave->id}}" class="btn btn-fab btn-fab-mini btn-raised btn-sm btn-danger" style="width:30px; height:30px; padding:11px 4px 10px 4px;"><i class="glyphicon glyphicon-trash"></i></button>
     </td>
+    {{Form::close()}}
     </tr>
     <?php $slno++; ?>
     @endforeach
--- app/views/user/index.blade.php
+++ app/views/user/index.blade.php
@@ -22,39 +22,33 @@ $(document).ready(function() {
 <div class="col-md-8">
 <div class="panel panel-default">
 <div class="panel-heading">
-	<div class="col-md-3">
-		<h5><strong>MANAGE USER</strong></h5>
-	</div>
-	<div class="col-md-3">
-		<input type='text' name='dzApparatusName' placeholder="Search By Name" class="form-control" value='<?php if(isset($_GET['dzGo'])){ echo $_GET['dzApparatusName'];} ?>'>
-	</div>
-    <input type="submit" name="dzGo" value="GO" class="btn btn-info btn-sm"/>
+    <h5><strong>MANAGE LEAVE</strong></h5>  
 </div>
 <div class="panel-body" style="padding:0px;">
 <table width="100%" border="0" cellspacing="0" cellpadding="0" class="table table-hover" style="margin-bottom:0px;">
 <thead>
   <tr>
-    <th width="85" height="38" align="center">#</th>
+    <th width="85" height="38" class="text-center">#</th>
     <th width="244" height="38" align="left">Name</th>
     <th width="244" height="38" align="left">Username</th>
     <th width="294" height="38" align="left">Email</th>
-    <th width="294" height="38" align="left">Type</th>
-    <th width="121" align="left">Control</th>
+    <th width="100" height="38" align="left">Type</th>
+    <th width="121" class="text-center">Control</th>
   </tr>
   </thead>
   <tbody>
   	<?php $slno = 0; ?>
   	@foreach($userAll as $user)
   	<tr bgcolor="">
-    <td height="25" align="center">{{$slno+$index}}</td>
+    <td height="25" class="text-center">{{$slno+$index}}</td>
     <td height="25" align="left">{{ $user->name }}&nbsp;</td>
     <td height="25" align="left">{{ $user->username }}&nbsp;</td>
     <td height="25" align="left" bgcolor="">{{ $user->email }}&nbsp;</td>
     <td height="25" align="left" bgcolor="">{{ $user->user_type }}&nbsp;</td>
     <td align="left" class="action text-center">
     	{{Form::open(array('url'=>route('user.destroy', array($user->id)),'method'=>'delete'))}}
-            <a href="{{route('user.edit', array($user->id))}}" class="btn btn-xs btn-success tooltip-top" title="Edit User"><i class="glyphicon glyphicon-edit"></i></a>&nbsp;&nbsp;
-            <button type="submit" onclick="return confirm ('<?php echo _('Are you sure') ?>');" name="id" class="btn btn-xs btn-danger tooltip-top" title="Remove User" value="{{$user->id}}"><i class="glyphicon glyphicon-trash"></i></button>
+            <a href="{{route('user.edit', array($user->id))}}" class="btn btn-fab btn-fab-mini btn-raised btn-sm btn-primary"  style="width:30px; height:30px; padding:11px 5px 10px 6px;" title="Edit User"><i class="glyphicon glyphicon-edit"></i></a>&nbsp;&nbsp;
+            <button type="submit" onclick="return confirm ('<?php echo _('Are you sure') ?>');" name="id" class="btn btn-fab btn-fab-mini btn-raised btn-sm btn-danger" style="width:30px; height:30px; padding:11px 4px 10px 4px;" title="Remove User" value="{{$user->id}}"><i class="glyphicon glyphicon-trash"></i></button>
         {{Form::close()}}
     </td>
     </tr>
